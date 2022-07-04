1f4652ed79cb08bad12f7a5fa10312ea06bc9db0
XF7BC <103952064+XF7BC@users.noreply.github.com>
2022-07-04 17:35:53 +0100

    New Upload and Major Tweaks
    added the newly design vendor sign up page and so on

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ function validateForm2(){
       alert("Last name must be filled out");
       return false;
     }
+    let f = document.forms["myform2"]["phone_number"].value;
+    if (f == "") {
+      alert("Enter your Phone Number");
+      return false;
+    }
     let c = document.forms["myform2"]["email_address"].value;
     if (c == "") {
       alert("Email must be filled out");
@@ -53,4 +58,84 @@ function validateForm3(){
   if(x == ""){
       alert ("Enter your Email address");
   }
+}
+
+
+
+function showPage(panelIndex, color){
+  var buttons= document.querySelectorAll("input.btn");
+ var pages = document.querySelectorAll("div.page");
+
+  buttons.forEach(function(node){
+   node.style.fontWeight='';
+  });
+  buttons[panelIndex].style.backgroundColor=color;
+  pages.forEach(function(node){
+    node.style.display="none"
+  });
+  pages[panelIndex].style.display="flex";
+
+}
+
+showPage(0);
+
+function validateForm4(){
+  let a = document.forms["myform4"]["branch_name"].value;
+  if (a == "") {
+    alert("Branch name must be filled out");
+    return false;
+  }
+  let b = document.forms["myform4"]["state"].value;
+  if (b == "") {
+    alert("State must be filled out");
+    return false;
+  }
+  let h = document.forms["myform4"]["city"].value;
+  if (h == "") {
+    alert("City must be filled out");
+    return false;
+  }
+  let f = document.forms["myform4"]["phone_number"].value;
+  if (f == "") {
+    alert("Enter your Phone Number");
+    return false;
+  }
+  let c = document.forms["myform4"]["email_address"].value;
+  if (c == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  let d = document.forms["myform4"]["password"].value;
+  if (d == "") {
+    alert("Enter a password");
+    return false;
+  }
+  let g = document.forms["myform4"]["t_c"].checked;
+  if (g == false) {
+    alert("Agree to the T's and C's.");
+    return false;
+  }
+  let e = document.forms["myform4"]["confirm_password"].value;
+  if (e == "") {
+    alert("Confirm your password");
+    return false;
+  }
+  if(e === d){
+      return true;
+  }else {
+      alert ("Passwords do not match!");
+      return false;
+  }
+}
+
+function validateForm3(){
+let x = document.forms["myform3"]["Email"].value;
+if(x == ""){
+    alert ("Enter your Email address");
+}
+}
+
+function popupMenu() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
