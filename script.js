3ab25fcb282186d263c9e079c90ecddf0b43a509
Swiftfoods <107245920+Swiftfoods@users.noreply.github.com>
2022-07-08 04:33:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function popupMenu() {
   popup.classList.toggle("show");
 }
 
-fetch('https://swiftfoodng-api.herokuapp.com/API/v1/auth/customers/login', {method:'GET', mode:'cors'})
+fetch('https://swiftfoodapi.herokuapp.com/API/v1/customers', {method:'GET', mode:'cors'})
 .then(res => {
   if (res.ok){
     console.log("success")
